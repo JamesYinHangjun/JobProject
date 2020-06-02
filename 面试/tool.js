ac5fdd27b530af2f818870707a1313a0fb2fcc46
JamesYin <3401754684@qq.com>
2020-06-03 00:07:30 +0800

    modify sth

--- 面试/tool.js
+++ 面试/tool.js
@@ -1,4 +1,4 @@
-// 验证码
+//1. 验证码
 // 纯数字验证码
  function numCode(n) {
     var arr = []
@@ -9,7 +9,7 @@
     return arr.join("")
 }
 
-数字和字母结合的验证码
+// 数字和字母结合的验证码
 function numCode(n) {
     var arr = []
     for(var i = 0; i < n; i++) {
@@ -24,3 +24,45 @@ function numCode(n) {
     }
     return arr.join("")
 }
+
+
+//2. 已知一个字符串对象中，英文字母由各种非字母字符分割，统计单词的个数
+// 如 "Yes, she*is%my#love"   返回 5
+function countOfWord(str) {
+    var count = 0
+    for(var i = 0; i < str.length-1; i++) {
+        if(isABC(str[i]) && !isABC(str[i+1])) {
+            count++
+        }
+    }
+    return count
+}
+// 判断一个字符是否是字母
+function isABC(charStr) {
+    if(charStr >= 'a' && charStr <= 'z' || charStr >= 'A' && charStr <= 'Z') {
+        return true
+    } else {
+        return false
+    }
+}
+// console.log(countOfWord('Yes, she*is%my#love.'))
+
+// 3.判断子字符串在长字符串中出现的次数
+// 如： 'abc'在'abcabcabc'中出现的次数
+function countOfStr(supStr,subStr) {
+    var arr = supStr.split(subStr)
+    return arr.length - 1
+}
+
+// console.log(countOfStr('abcabcabc','abc'))
+
+
+// 4.判断单个字符是否是数字字母下划线
+function isDEF(charStr) {
+    if(charStr >= 'a' && charStr <= 'z' || charStr >= 'A' && charStr <= 'Z' || charStr >= '0' && charStr <= '9' || charStr == '_') {
+        return true
+    } else{
+        return false
+    }
+}
+// console.log(isDEF('&'))
